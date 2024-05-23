54ccfaf28247483680bf6845b3a0432ca93ba39e
sakuexe <saku.karttunen@gmail.com>
2024-05-23 10:14:58 +0300

    Raino now handles multiple attachments! 🎉
    - Added support for multiple attachments in the `convert` command
    - The attachments get handled in parallel using goroutines and a channel
    - In case of an error, Raino will send the successful conversions and a
      message with each of the errors that he came across

--- internal/handlers/convert.go
+++ internal/handlers/convert.go
@@ -3,15 +3,85 @@ package handlers
 import (
 	"bytes"
 	"fmt"
+	"sync"
 
 	"github.com/bwmarrin/discordgo"
 	imageconversion "github.com/sakuexe/go-raino/internal/image-conversion"
 )
 
+var messageHistoryLimit int = 100
+
+func findAttachments(session *discordgo.Session,
+	interaction *discordgo.Interaction) ([]*discordgo.MessageAttachment, error) {
+
+	// get the user that sent the command
+	user := interaction.Member.User
+
+	// get all the messages in the channel (up to the set limit)
+	messages, _ := session.ChannelMessages(interaction.ChannelID, messageHistoryLimit, "", "", "")
+
+	// look for the newest message from the user that has an attachment
+	var message *discordgo.Message = nil
+	// loop in reverse order (newest to oldest)
+	for i := len(messages) - 1; i >= 0; i-- {
+		msg := messages[i]
+		if msg.Author.ID == user.ID && len(msg.Attachments) > 0 {
+			message = msg
+		}
+	}
+
+	if message == nil {
+		rainoMessage := fmt.Sprintf("I looked at the last %d messages and didn't find yours with attachments, %s",
+			messageHistoryLimit, user.Username)
+		return nil, fmt.Errorf(rainoMessage)
+	}
+
+	// get the image from the message
+	return message.Attachments, nil
+}
+
+func convertAttachmentsParallel(attachments []*discordgo.MessageAttachment, format string) ([]*discordgo.File, []error) {
+	files := []*discordgo.File{}
+	waitGroup := sync.WaitGroup{}
+  // create a channel to send errors to
+	errChannel := make(chan error, len(attachments))
+
+	for index, attachment := range attachments {
+		waitGroup.Add(1)
+		go func(index int, attachment *discordgo.MessageAttachment) {
+			defer waitGroup.Done()
+			imageResponse, err := imageconversion.ConvertImage(format, attachment.URL)
+
+			// if there was an error, send it to the error channel and return
+			if err != nil {
+				errorMessage := fmt.Errorf("**Error with image %d**: %s", index+1, err.Error())
+				errChannel <- errorMessage
+				return
+			}
+
+			reader := bytes.NewReader(imageResponse.Buffer.Bytes())
+			files = append(files, &discordgo.File{
+				Name:        imageResponse.Filename,
+				ContentType: imageResponse.ContentType,
+				Reader:      reader,
+			})
+		}(index, attachment)
+	}
+
+  // wait for all the goroutines to finish
+	waitGroup.Wait()
+	close(errChannel)
+
+  // get all the errors from the channel
+  errors := []error{}
+  for err := range errChannel {
+    errors = append(errors, err)
+  }
+	return files, errors
+}
+
 func convertHandler(session *discordgo.Session, interaction *discordgo.InteractionCreate) {
-	// customize some variables
 	var format string = interaction.ApplicationCommandData().Options[0].StringValue()
-	var messageHistoryLimit int = 100
 
 	// add a status for the process
 	status := fmt.Sprintf("Converting an image to %s", format)
@@ -29,64 +99,28 @@ func convertHandler(session *discordgo.Session, interaction *discordgo.Interacti
 		})
 	}
 
-	// get the user that sent the command
-	user, err := session.User(interaction.Interaction.Member.User.ID)
+	attachments, err := findAttachments(session, interaction.Interaction)
 	if err != nil {
-		fmt.Println("Couldn't get the user that sent the command")
-		session.FollowupMessageCreate(interaction.Interaction, true, &discordgo.WebhookParams{
-			Content: "Couldn't get the user that sent the command",
-		})
-		return
-	}
-
-	// get user's messages in the channel
-	messages, _ := session.ChannelMessages(interaction.Interaction.ChannelID, messageHistoryLimit, "", "", "")
-	// look for the message that is from the user and has the attachment
-	var message *discordgo.Message = nil
-	// loop in reverse order (newest to oldest)
-	for i := len(messages) - 1; i >= 0; i-- {
-		msg := messages[i]
-		if msg.Author.ID == user.ID && len(msg.Attachments) > 0 {
-			message = msg
-		}
-	}
-	if message == nil {
-		fmt.Printf("Couldn't a message with attachments from the user %s. Looked at the last 100 messages.\n", user.Username)
-		rainoMessage := fmt.Sprintf("I looked at the last 100 messages and didn't find yours with attachments, %s", user.Username)
-		session.FollowupMessageCreate(interaction.Interaction, true, &discordgo.WebhookParams{
-			Content: rainoMessage,
-		})
-		return
-	}
-
-	// get the image from the message
-	attachmentUrl := message.Attachments[0].URL
-
-	imageResponse, err := imageconversion.ConvertImage(format, attachmentUrl)
-	if err != nil {
-		fmt.Println("Error converting the image: ", err)
 		session.FollowupMessageCreate(interaction.Interaction, true, &discordgo.WebhookParams{
 			Content: err.Error(),
 		})
 		return
 	}
 
-	// read the image file
-	reader := bytes.NewReader(imageResponse.Buffer.Bytes())
-	// fmt.Println("Filename: ", imageResponse.Filename)
-	// fmt.Println("Content Type: ", imageResponse.ContentType)
-	// fmt.Println("Filepath: ", imageResponse.Filepath)
+	files, errors := convertAttachmentsParallel(attachments, format)
 
 	// send a follow up message
+	user := interaction.Member.User
 	var content string = fmt.Sprintf("Here you go, <@%s>:", user.ID)
+
+	if len(errors) > 0 {
+		for _, err := range errors {
+			content += fmt.Sprintf("\n%s", err.Error())
+		}
+	}
+
 	session.FollowupMessageCreate(interaction.Interaction, true, &discordgo.WebhookParams{
 		Content: content,
-		Files: []*discordgo.File{
-			{
-				Name:        imageResponse.Filename,
-				ContentType: imageResponse.ContentType,
-				Reader:      reader,
-			},
-		},
+		Files:   files,
 	})
 }
