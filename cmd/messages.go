caf4c3f2cb8720e39b18a3af9cf4ce250b2adad4
sakuexe <saku.karttunen@gmail.com>
2024-05-26 12:39:26 +0300

    small tweak to hey raino answers

--- cmd/messages.go
+++ cmd/messages.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/sakuexe/go-raino/internal/openai"
@@ -19,7 +20,7 @@ func askMessageHandler(session *discordgo.Session, message *discordgo.MessageCre
 	// make a string of the last 10 messages with usernames
 	context := fmt.Sprintf("here are the last %d messages of the discord chat. " +
 		"Use them as the context for the latest message that you are replying to. " +
-		"Only include your message in the response, not your username\n\n", messageHistorySize)
+		"\n\n", messageHistorySize)
 
 	// iterate through the messages from oldest to newest
 	for index := len(messages) - 1; index >= 0; index-- {
@@ -29,9 +30,13 @@ func askMessageHandler(session *discordgo.Session, message *discordgo.MessageCre
 
 	response, err := openai.SendChat(context)
 	if err != nil {
-		fmt.Println("Error generating gpt response: ", err)
 		session.ChannelMessageSend(message.ChannelID, err.Error())
 	}
+
+  // if the message starts with Raino: then remove it
+  if strings.HasPrefix(response.Choices[0].Message.Content, "Raino:") {
+    return strings.TrimPrefix(response.Choices[0].Message.Content, "Raino:")
+  }
 
   return response.Choices[0].Message.Content
 }
